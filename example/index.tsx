24015092fc70fcf9abb0097bc52085a30cc28eb3
Nam Nguyen <namnguyen@axon.com>
2019-09-16 18:23:31 +0700

    Add video controller

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import 'react-app-polyfill/ie11';
 import 'flexibility';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
@@ -26,6 +26,18 @@ const zoomImageUrl = 'https://picsum.photos/id/1037/1024/768';
 // const zoomImageUrl = 'https://picsum.photos/id/1037/800/5000';
 
 const App = () => {
+  const [isPlay, setIsPlay] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const togglePlay = () => {
+    const video = videoRef.current as HTMLVideoElement;
+    setIsPlay(!isPlay);
+    if (isPlay) {
+      video.pause();
+    } else {
+      video.play();
+    }
+  };
   return (
     <Wrapper>
       <Outer>
@@ -39,21 +51,20 @@ const App = () => {
           <ZoomableVideo
             renderVideoWrapper={videoWrapperRef => {
               return (
-                <div ref={videoWrapperRef}>
-                  <video
-                    style={{
-                      height: 'auto',
-                      width: '100%',
-                    }}
-                    controls
-                    // src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_5MB.mp4"
-                    src="http://127.0.0.1:8080/video.mp4"
-                  />
-                </div>
+                <video
+                  style={{
+                    height: 'auto',
+                    width: '100%',
+                  }}
+                  ref={videoRef}
+                  // src="https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/1080/Big_Buck_Bunny_1080_10s_5MB.mp4"
+                  src="http://127.0.0.1:8080/video.mp4"
+                />
               );
             }}
           />
         </Zoomable>
+        <button onClick={togglePlay}>{isPlay ? 'Pause' : 'Play'}</button>
       </Outer>
     </Wrapper>
   );
