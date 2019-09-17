0d298da7811a9c30de9587ff38daf2b3b18ccfe2
Nam Nguyen <namnguyen@axon.com>
2019-09-17 11:53:40 +0700

    Clean up code

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,26 @@
-export enum ZOOM_DIRECTION {
-  IN = 'IN',
-  OUT = 'OUT',
+import { ZOOM_DIRECTION } from 'constant';
+
+export interface ZoomableContextType {
+  height: number;
+  width: number;
+  currentZoom: number;
+  lastPositionX: number;
+  lastPositionY: number;
+  positionX: number;
+  positionY: number;
+  isOnMove: boolean;
+  startX: number;
+  startY: number;
+  percentage: number;
+  setPercentage: (newCurrentZoom: number) => void;
+  onImageLoad: () => void;
+  onVideoLoad: () => void;
+  zoom: (direction: ZOOM_DIRECTION) => () => void;
+  onWheel: (event: WheelEvent) => void;
+  handleMouseUp: () => void;
+  handleMouseDown: (event: React.MouseEvent<HTMLDivElement>) => void;
+  handleMouseMove: (event: React.MouseEvent<HTMLDivElement>) => void;
+  wrapperRef: React.RefObject<HTMLDivElement>;
+  imageRef: React.RefObject<HTMLImageElement>;
+  sliderRef: React.RefObject<HTMLDivElement>;
 }
--- src/zoomContext.tsx
+++ src/zoomContext.tsx
@@ -1,30 +1,5 @@
 import React from 'react';
-import { ZOOM_DIRECTION } from 'types';
-
-export interface ZoomableContextType {
-  height: number;
-  width: number;
-  currentZoom: number;
-  lastPositionX: number;
-  lastPositionY: number;
-  positionX: number;
-  positionY: number;
-  isOnMove: boolean;
-  startX: number;
-  startY: number;
-  percentage: number;
-  setPercentage: (newCurrentZoom: number) => void;
-  onImageLoad: () => void;
-  onVideoLoad: () => void;
-  zoom: (direction: ZOOM_DIRECTION) => () => void;
-  onWheel: (event: WheelEvent) => void;
-  handleMouseUp: () => void;
-  handleMouseDown: (event: React.MouseEvent<HTMLDivElement>) => void;
-  handleMouseMove: (event: React.MouseEvent<HTMLDivElement>) => void;
-  wrapperRef: React.RefObject<HTMLDivElement>;
-  imageRef: React.RefObject<HTMLImageElement>;
-  sliderRef: React.RefObject<HTMLDivElement>;
-}
+import { ZoomableContextType } from 'types';
 
 const zoomableContext = React.createContext<ZoomableContextType | null>(null);
 
