d75cd28972bbdb9095ba87a12551aa1e61290631
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-09-18 14:42:11 +0700

    Convert ZoomableImage to hook

--- src/ZoomableImage.tsx
+++ src/ZoomableImage.tsx
@@ -1,94 +1,84 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 import { ZoomableContextType, withZoomableContext } from './ZoomableContext';
 
 interface ImageProps {
   imageUrl: string;
-  loading: React.ReactNode;
+  loading?: React.ReactNode;
   zoomContext: ZoomableContextType;
 }
 
 export default withZoomableContext(
-  class extends React.Component<ImageProps> {
-    state = { isLoading: true };
+  ({
+    imageUrl,
+    loading,
+    zoomContext: {
+      onImageLoad,
+      onWheel,
+      width,
+      height,
+      imageRef,
+      wrapperRef,
+      currentZoom,
+      sliderRef,
+      handleMouseDown,
+      handleMouseMove,
+      handleMouseUp,
+      positionX,
+      positionY,
+    },
+  }: ImageProps) => {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
-    componentDidMount() {
-      const {
-        zoomContext: { imageRef, sliderRef, onImageLoad, onWheel },
-      } = this.props;
-
+    useLayoutEffect(() => {
       const image = imageRef.current as HTMLImageElement;
       const slider = sliderRef.current as HTMLDivElement;
 
       image.onload = () => {
         onImageLoad();
-        this.setState({ isLoading: false });
+        setIsLoading(true);
       };
 
       slider.addEventListener('wheel', onWheel);
-    }
+    }, []);
 
-    render() {
-      const { zoomContext, imageUrl, loading } = this.props;
-      const { isLoading } = this.state;
-      if (!zoomContext) {
-        return null;
-      }
-
-      const renderedLoading = loading ? loading : <p>loading...</p>;
-
-      const {
-        width,
-        height,
-        imageRef,
-        wrapperRef,
-        currentZoom,
-        sliderRef,
-        handleMouseDown,
-        handleMouseMove,
-        handleMouseUp,
-        positionX,
-        positionY,
-      } = zoomContext;
-
-      return (
+    return (
+      <div
+        style={{
+          zIndex: 1312,
+          backgroundColor: '#000',
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          overflow: 'hidden',
+        }}
+        ref={wrapperRef}
+      >
         <div
+          ref={sliderRef}
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+          onMouseMove={handleMouseMove}
           style={{
-            zIndex: 1312,
-            backgroundColor: '#000',
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            overflow: 'hidden',
+            transformOrigin: '0 0',
+            cursor: 'move',
+            width,
+            height,
+            transform: `translate(${positionX}px, ${positionY}px) scale(${currentZoom})`,
           }}
-          ref={wrapperRef}
         >
-          <div
-            ref={sliderRef}
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}
+          <img
             style={{
-              transformOrigin: '0 0',
-              cursor: 'move',
               width,
               height,
-              transform: `translate(${positionX}px, ${positionY}px) scale(${currentZoom})`,
             }}
-          >
-            <img
-              style={{
-                width,
-                height,
-              }}
-              ref={imageRef}
-              src={imageUrl}
-            />
-          </div>
-          {isLoading && renderedLoading}
+            ref={imageRef}
+            src={imageUrl}
+          />
         </div>
-      );
-    }
+        {isLoading && loading}
+      </div>
+    );
   }
 );
