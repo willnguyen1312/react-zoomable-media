baafea72b7d8a922334efebe687bae2b31f0646d
Nam Nguyen <namnguyen@axon.com>
2019-09-18 12:05:55 +0700

    Convert ZoomableVideo to hook

--- src/ZoomableVideo.tsx
+++ src/ZoomableVideo.tsx
@@ -1,57 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ZoomableContextType, withZoomableContext } from './ZoomableContext';
 
 interface ImageProps {
   zoomContext: ZoomableContextType;
+  children: React.ReactNode;
 }
 
-export default withZoomableContext(
-  class extends React.Component<ImageProps> {
-    componentDidMount() {
-      const {
-        zoomContext: { sliderRef, onWheel },
-      } = this.props;
+export default withZoomableContext(({ zoomContext, children }: ImageProps) => {
+  const {
+    wrapperRef,
+    currentZoom,
+    sliderRef,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp,
+    positionX,
+    positionY,
+    onWheel,
+  } = zoomContext;
 
-      const slider = sliderRef.current as HTMLDivElement;
+  useEffect(() => {
+    const slider = sliderRef.current as HTMLDivElement;
 
-      slider.addEventListener('wheel', onWheel);
-    }
+    slider.addEventListener('wheel', onWheel);
+  }, []);
 
-    render() {
-      const { zoomContext, children } = this.props;
-      if (!zoomContext) {
-        return null;
-      }
-
-      const {
-        wrapperRef,
-        currentZoom,
-        sliderRef,
-        handleMouseDown,
-        handleMouseMove,
-        handleMouseUp,
-        positionX,
-        positionY,
-      } = zoomContext;
-
-      return (
-        <div style={{ width: '100%', overflow: 'hidden' }} ref={wrapperRef}>
-          <div
-            ref={sliderRef}
-            onMouseDown={handleMouseDown}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}
-            style={{
-              transformOrigin: '0 0',
-              cursor: 'move',
-              width: '100%',
-              transform: `translate(${positionX}px, ${positionY}px) scale(${currentZoom})`,
-            }}
-          >
-            {children}
-          </div>
-        </div>
-      );
-    }
-  }
-);
+  return (
+    <div style={{ width: '100%', overflow: 'hidden' }} ref={wrapperRef}>
+      <div
+        ref={sliderRef}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        style={{
+          transformOrigin: '0 0',
+          cursor: 'move',
+          width: '100%',
+          transform: `translate(${positionX}px, ${positionY}px) scale(${currentZoom})`,
+        }}
+      >
+        {children}
+      </div>
+    </div>
+  );
+});
