5ae5664a39fb10a97d5294afecafba0e9996989f
Nam Nguyen <nam.nguyen.node@gmail.com>
2020-01-09 11:48:38 +0700

    Update dependencies

--- src/Zoomable.tsx
+++ src/Zoomable.tsx
@@ -537,7 +537,7 @@ export const Zoomable: FC<ZoomableProps> = ({
       dh
     );
 
-    imageDataCallbackHandler(canvas.toDataURL('image/png', 1.0));
+    imageDataCallbackHandler(canvas.toDataURL());
   };
 
   return (
